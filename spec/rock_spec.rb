7ca21dce68def999807957e8c92a2f9c5a32afe3
Adam Zinder and Luke Philips <luke@>
2016-11-16 12:09:01 -0800

    integration testing in process

--- spec/rock_spec.rb
+++ spec/rock_spec.rb
@@ -5,6 +5,17 @@ require('rock')
 Capybara.app = Sinatra::Application
 set(:show_excpetions, false)
 
+describe('the rock papers scissors path', {:type => :feature}) do
+  it ("processes the user entry and returns the game outcome") do
+    visit('/')
+     select('Rock', :from => 'user_choice')
+     click_button('Play')
+    expect(page).to have_content(0)
+    expect(page).to have_content(1)
+    expect(page).to have_content(2)
+  end
+end
+
 describe('String#rock_score') do
   it ('displays the users choice') do
     expect("rock".rock_score()).to(be_between(0, 2))
